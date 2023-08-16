196c612a7060cadb134fcbf8da3376f2bec615d7
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-08-16 14:01:55 +0200

    Fixed bug

--- src/index.ts
+++ src/index.ts
@@ -129,7 +129,7 @@ export class Game {
 
 function createAction<canDisposeValueCard extends boolean>(game: Game, addDisposePileToDeck: Game['addDisposePileToDeck'], handCards: Game['handCards'], replaceHandCard: Game['replaceHandCard'], drawnCard: Card, canDisposeValueCard: canDisposeValueCard): action<ActivePlayer, Card, canDisposeValueCard, 'finished'> {
     const newAction: action<ActivePlayer, Card, canDisposeValueCard, 'new'>
-        = game.currentActivePlayer.performAction(drawnCard, canDisposeValueCard, this.previousActions, game.state === 'lastRound', game.disposePile, game);
+        = game.currentActivePlayer.performAction(drawnCard, canDisposeValueCard, game.previousActions, game.state === 'lastRound', game.disposePile, game);
 
     if (newAction.performer !== game.currentActivePlayer)
         throw new Error('Performer of returned action isn\'t self');
