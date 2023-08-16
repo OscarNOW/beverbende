6c5bf17216e75350269ba4b6156da054754257d2
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-08-16 13:47:52 +0200

    Created debug player

---
+++ players/Debug.ts
@@ -0,0 +1,67 @@
+import { ActivePlayer, Card, Player, action, privateInformation } from '../src/statics';
+import { Game } from '../src/index';
+
+export class Debug extends Player {
+    constructor() {
+        super('Debug');
+    }
+
+    performAction<canDisposeValueCard extends boolean, activePlayer extends ActivePlayer, drawnCard extends Card>(
+        drawnCard: drawnCard,
+        canDisposeValueCard: canDisposeValueCard,
+        activePlayer: activePlayer,
+        previousActions: action<ActivePlayer, Card, true, 'finished'>[],
+        isLastRound: boolean,
+        privateInformation: privateInformation<activePlayer['privateInformationKeys']>,
+        disposePile: Card[],
+        game: Game
+    ): action<activePlayer, drawnCard, canDisposeValueCard, 'new'> {
+        if (drawnCard.isActionCard)
+            if (drawnCard.action === 'extraDraw')
+                return {
+                    performer: activePlayer,
+                    type: 'extraDraw',
+                    drawnCardLocation: 'dispose',
+                    drawnCard: drawnCard,
+                    actions: []
+                }
+            else if (drawnCard.action === 'look')
+                return {
+                    performer: activePlayer,
+                    type: 'look',
+                    drawnCardLocation: 'dispose',
+                    drawnCard,
+
+                    cardSlot: activePlayer.hand[0]
+                }
+            else if (drawnCard.action === 'switch')
+                return {
+                    performer: activePlayer,
+                    type: 'switch',
+                    drawnCardLocation: 'dispose',
+                    drawnCard,
+
+                    ownCardSlot: activePlayer.hand[0],
+                    otherCardSlot: game.activePlayers.find(p => p !== activePlayer).hand[0]
+                }
+            else
+                throw new Error(`Unknown action: "${drawnCard.action}"`)
+        else if (drawnCard.isActionCard === false)
+            if (canDisposeValueCard)
+                return {
+                    performer: activePlayer,
+                    type: 'dispose',
+                    drawnCardLocation: 'dispose',
+                    drawnCard
+                }
+            else
+                return {
+                    performer: activePlayer,
+                    type: 'use',
+                    drawnCardLocation: 'hand',
+                    cardSlot: activePlayer.hand[0]
+                }
+        else
+            throw new Error(`Unknown isActionCard: "${drawnCard.isActionCard}"`)
+    }
+}
