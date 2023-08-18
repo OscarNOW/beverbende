b8f8bad0a994da190bdb1a9bf4051c10715febd3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-08-18 23:53:33 +0200

    Working on WebPlayer

--- players/Web/site.ts
+++ players/Web/site.ts
@@ -1,17 +1,57 @@
 const port = 4000;
 
 import express from 'express';
+import { ActionCard, ActivePlayer, Card, action, privateInformation } from '../../src/statics';
+import { Game } from '../../src';
+import { Web as WebPlayer } from './index';
 
 const app = express();
+app.use('/files', express.static('./client/files'));
+
 app.get('/', (req, res) => {
-    res.send('Welkom! Gebruik de specifieke link voor de speler om te starten.')
+    res.sendFile('./client/home.html', { root: __dirname });
+});
+
+app.get('/player/:id', (req, res) => {
+    res.sendFile('./client/player.html', { root: __dirname });
 });
 
 app.listen(port, () => {
     console.log('Web player listening on port 4000')
 });
 
-app.get('/player/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(id);
-});
+const webPlayers: WebPlayer[] = [];
+export function addPlayer(webPlayer: WebPlayer): void {
+    webPlayers.push(webPlayer);
+}
+
+export async function performAction<canDisposeValueCard extends boolean, activePlayer extends ActivePlayer, drawnCard extends Card>(
+    webPlayer: WebPlayer,
+    drawnCard: drawnCard,
+    canDisposeValueCard: canDisposeValueCard,
+    activePlayer: activePlayer,
+    privateInformation: privateInformation<activePlayer['privateInformationKeys']>,
+    game: Game
+): action<activePlayer, drawnCard, canDisposeValueCard, 'new'> {
+    //todo-imp
+}
+
+export async function declareLastRound<activePlayer extends ActivePlayer>(
+    webPlayer: WebPlayer,
+    activePlayer: activePlayer,
+    privateInformation: privateInformation<activePlayer['privateInformationKeys']>,
+    game: Game
+): boolean {
+    //todo-imp
+}
+
+export async function acceptExtraDrawCard<activePlayer extends ActivePlayer, drawnCard extends Card>(
+    webPlayer: WebPlayer,
+    drawnCard: drawnCard,
+    activePlayer: activePlayer,
+    currentAction: action<activePlayer, ActionCard<'extraDraw'>, true, 'current'>,
+    privateInformation: privateInformation<activePlayer['privateInformationKeys']>,
+    game: Game
+): boolean {
+    //todo-imp
+}
