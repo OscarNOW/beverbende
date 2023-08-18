f7da0de6c7fa4b00e1c6971db091fc93e06642fb
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-08-18 18:05:45 +0200

    Try catch around box plot

--- index.ts
+++ index.ts
@@ -28,39 +28,43 @@ function sum(array: number[]): number {
     return array.reduce((a, b) => a + b, 0);
 }
 
-// Create boxplot
-console.log();
-const plotly = require('plotly')('OscarNOW', require('../secret.json').plotly);
+try {
+    // Create boxplot
+    console.log();
+    const plotly = require('plotly')('OscarNOW', require('../secret.json').plotly);
 
-const data = [
-    {
-        y: aPoints,
-        boxpoints: 'all',
-        jitter: 0.4,
-        pointpos: 0,
-        type: 'box'
-    },
-    {
-        y: bPoints,
-        boxpoints: 'all',
-        jitter: 0.4,
-        pointpos: 0,
-        type: 'box'
-    }
-];
-const graphOptions = { filename: 'box-plot', fileopt: 'overwrite' };
+    const data = [
+        {
+            y: aPoints,
+            boxpoints: 'all',
+            jitter: 0.4,
+            pointpos: 0,
+            type: 'box'
+        },
+        {
+            y: bPoints,
+            boxpoints: 'all',
+            jitter: 0.4,
+            pointpos: 0,
+            type: 'box'
+        }
+    ];
+    const graphOptions = { filename: 'box-plot', fileopt: 'overwrite' };
 
-plotly.plot(data, graphOptions, function (err: any, msg: {
-    streamStatus: undefined;
-    url: string;
-    message: string;
-    warning: string;
-    filename: string;
-    error: string;
-}) {
-    if (err) throw err;
-    else if (msg.error.length !== 0) throw msg.error;
-    else if (msg.warning.length !== 0) console.warn(msg.warning);
+    plotly.plot(data, graphOptions, function (err: any, msg: {
+        streamStatus: undefined;
+        url: string;
+        message: string;
+        warning: string;
+        filename: string;
+        error: string;
+    }) {
+        if (err) throw err;
+        else if (msg.error.length !== 0) throw msg.error;
+        else if (msg.warning.length !== 0) console.warn(msg.warning);
 
-    console.log(msg.url);
-});
+        console.log(msg.url);
+    });
+} catch {
+    console.warn('Box plot failed');
+}
