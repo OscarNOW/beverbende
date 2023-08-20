f7910b5d32c95211e049d39afb53dd699d1e3d5f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-08-20 19:29:28 +0200

    Added circular json to build

--- players/Web/client/build.bat
+++ players/Web/client/build.bat
@@ -2,20 +2,27 @@
 
 cd ../../..
 
-if not exist node_modules/socket.io-client/dist (
-    echo node_modules/socket.io-client/dist not exist
+if not exist node_modules/socket.io-client/dist/socket.io.esm.min.js (
+    echo node_modules/socket.io-client/dist/socket.io.esm.min.js not exist
     goto :end
 )
 cd node_modules/socket.io-client/dist
-
 copy "socket.io.esm.min.js" "../../../dist/players/web/client/files/socket.io-copy-client.js"
+cd ../../..
 
+if not exist node_modules/circular-json-es6/index.js (
+    echo node_modules/circular-json-es6/index.js not exist
+    goto :end
+)
+cd node_modules/circular-json-es6
+copy "index.js" "../../dist/players/web/client/files/circular-json-copy"
 cd ../../..
 
 
 cd dist/players/web/client/files/
 
 powershell -Command "(gc ws.js) -replace 'socket.io-client', '/files/socket.io-copy-client.js' | Out-File -encoding ASCII ws.js"
+powershell -Command "(gc ws.js) -replace 'circular-json-es6', '/files/circular-json-copy.js' | Out-File -encoding ASCII ws.js"
 
 if exist player.js (
     if exist copy-player.js ( del copy-player.js )
