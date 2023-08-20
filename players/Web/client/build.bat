3d921e4741fdb91cca9d45c0ec719a46d0994bf3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-08-20 19:40:28 +0200

    Revert "Removed circular json from build"
    This reverts commit bd6ddf51b4feb11cc5c9555760957c7cf5bb6d46.

--- players/Web/client/build.bat
+++ players/Web/client/build.bat
@@ -10,8 +10,18 @@ cd node_modules/socket.io-client/dist
 copy "socket.io.esm.min.js" "../../../dist/players/web/client/files/socket.io-copy-client.js"
 cd ../../..
 
+if not exist node_modules/circular-json-es6/index.js (
+    echo node_modules/circular-json-es6/index.js not exist
+    goto :end
+)
+cd node_modules/circular-json-es6
+copy "index.js" "../../dist/players/web/client/files/circular-json-copy.js"
+cd ../..
+
+
 cd dist/players/web/client/files/
 
+powershell -Command "(gc ws.js) -replace 'socket.io-client', '/files/socket.io-copy-client.js' | Out-File -encoding ASCII ws.js"
 powershell -Command "(gc ws.js) -replace 'circular-json-es6', '/files/circular-json-copy.js' | Out-File -encoding ASCII ws.js"
 
 if exist player.js (
